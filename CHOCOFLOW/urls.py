56b2f46642c790c6ca2ada78a41ae7d93698b8cf
Reishell Castro <castromatallana2006@gmail.com>
2026-04-28 17:06:20 -0500

    Actualización: corrección de estilos y configuración static

--- CHOCOFLOW/urls.py
+++ CHOCOFLOW/urls.py
@@ -1,6 +1,8 @@
+from django.contrib import admin
 from django.urls import path
-from .views import index
+from myApp.views import index  # ✅ IMPORT CORRECTO
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', index, name='index'),
 ]
